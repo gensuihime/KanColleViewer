90ab38610bb986934f5e0b1e558d3370c9fb696f
gensuihime <gensuihime@gmail.com>
2015-05-11 18:04:22 +0900

    Modification
    Changes

--- Plugins/BattlePreview/Properties/AssemblyInfo.cs
+++ Plugins/BattlePreview/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // 어셈블리의 일반 정보는 다음 특성 집합을 통해 제어됩니다. 
 // 어셈블리와 관련된 정보를 수정하려면
 // 이 특성 값을 변경하십시오.
-[assembly: AssemblyTitle("제독업무도 바빠! 전투예보 출력 플러그인")]
-[assembly: AssemblyDescription("전투계산결과를 출력하는 플러그인입니다. 현 버전은 한글버전에서만 작동합니다.")]
+[assembly: AssemblyTitle("KCV Battle Result Prediction Plugin")]
+[assembly: AssemblyDescription("This plugin outputs the results from a battle. Currently known to work only on this fork.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KCVkr.tistory.com")]
 [assembly: AssemblyProduct("KanColleViewer")]
-[assembly: AssemblyCopyright("Copyright © 2015 FreyYa")]
+[assembly: AssemblyCopyright("Copyright © 2015 FreyYa, Forked by Gensuihime")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
